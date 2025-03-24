e29e246d423648d705dec2dc5ab804998b47d3d0
Dhansca <49760606+Dhansca@users.noreply.github.com>
2025-03-24 19:59:59 -0400

    Update main.tf
    Correct TF errors

--- staging/main.tf
+++ staging/main.tf
@@ -17,7 +17,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "GHAEC2" {
-
   ami                    = "ami-0596d807260f25fc1"
   instance_type          = "t2.micro"
   tags                   = var.tags
