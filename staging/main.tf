99fe7041349ad2e5fcebf2a51696cbf2a3a1e714
Dhansca <49760606+Dhansca@users.noreply.github.com>
2025-03-24 20:06:16 -0400

    Update main.tf
    Removal Tags

--- staging/main.tf
+++ staging/main.tf
@@ -19,14 +19,7 @@ provider "aws" {
 resource "aws_instance" "GHAEC2" {
     ami                    = "ami-0596d807260f25fc1"
     instance_type          = "t2.micro"
-    tags                   = 
-      {
-        name = "GHA-ACS730"
-        env  = "test"
-      }
-}
-
-
+  }
 
 resource "random_pet" "sg" {}
 
