c77c72d9f1243cc5d8754474fe209471df655c84
Dhansca <49760606+Dhansca@users.noreply.github.com>
2025-03-24 20:00:59 -0400

    Update main.tf
    TF code

--- prod/main.tf
+++ prod/main.tf
@@ -17,14 +17,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "GHAEC2" {
-
-  ami                    = "ami-0596d807260f25fc1"
-  instance_type          = "t2.micro"
-  tags                   = var.tags
-  {
-    name = "GHA-ACS730"
-    env  = "test"
-  }
+    ami                    = "ami-0596d807260f25fc1"
+    instance_type          = "t2.micro"
+    tags                   = var.tags
+      {
+        name = "GHA-ACS730"
+        env  = "test"
+      }
 }
 
 resource "random_pet" "sg" {}
